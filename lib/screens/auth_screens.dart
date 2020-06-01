7ffc77686916498cc7c45d69a0cd5efcb50ce98b
triyono <yono.tri@gmail.com>
2020-06-02 05:51:24 +0700

    add firebase auth

--- lib/screens/auth_screens.dart
+++ lib/screens/auth_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter_chat/widgets/auth/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthScreen extends StatefulWidget {
   @override
@@ -8,8 +9,23 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _auth = FirebaseAuth.instance;
   void _submitAuthForm(
-      String email, String password, String username, bool isLogin) {}
+    String email,
+    String password,
+    String username,
+    bool isLogin,
+  ) async {
+    AuthResult authResult;
+    if (isLogin) {
+      authResult = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+    } else {
+      authResult = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
