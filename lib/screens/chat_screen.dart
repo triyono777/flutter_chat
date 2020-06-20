c78b783b34a1e2f214751d6be33c53bebe3cf844
triyono <yono.tri@gmail.com>
2020-06-20 20:39:09 +0700

    fix tampilan

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -13,6 +13,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   void initState() {
+    super.initState();
     // untuk IOS ini wajib, sebagai permission
     final fbm = FirebaseMessaging();
     fbm.requestNotificationPermissions();
@@ -28,7 +29,6 @@ class _ChatScreenState extends State<ChatScreen> {
     });
 //    fbm.getToken();
     fbm.subscribeToTopic('chat');
-    super.initState();
   }
 
   @override
@@ -38,6 +38,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text('FlutterChat'),
         actions: [
           DropdownButton(
+            underline: Container(),
             icon: Icon(
               Icons.more_vert,
               color: Theme.of(context).primaryIconTheme.color,
